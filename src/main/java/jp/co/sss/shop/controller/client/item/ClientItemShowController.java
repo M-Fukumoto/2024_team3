db7f7834b643402d97849fc8c8bfb5a22f0a4daa
koteiketsu <koteiketsu@jcyu.com>
2024-06-13 13:14:13 +0900

    商品詳細表示コントローラ作成完了

--- src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
+++ src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
@@ -3,11 +3,15 @@ package jp.co.sss.shop.controller.client.item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jp.co.sss.shop.bean.ItemBean;
+import jp.co.sss.shop.entity.Item;
 import jp.co.sss.shop.repository.ItemRepository;
 import jp.co.sss.shop.service.BeanTools;
+import jp.co.sss.shop.util.Constant;
 
 /**
  * 商品管理 一覧表示機能(一般会員用)のコントローラクラス
@@ -36,8 +40,37 @@ public class ClientItemShowController {
 	 */
 	@RequestMapping(path = "/" , method = { RequestMethod.GET, RequestMethod.POST })
 	public String index(Model model) {
-	
-		return "index";
+		
+	return "index";
 	}
+	
+	
+	/**
+	 * 商品情報詳細表示処理
+	 *
+	 * @param id  商品ID
+	 * @param model  Viewとの値受渡し
+	 * @return "client/item/detail" 詳細画面 表示
+	 */
+	@RequestMapping(path = "/client/item/detail/{id}", method = RequestMethod.GET)
+	public String showItem(@PathVariable int id, Model model) {
 
+		// 対象の商品情報を取得
+		Item item = itemRepository.findByIdAndDeleteFlag(id, Constant.NOT_DELETED);
+
+	if (item == null) {
+			// 対象が無い場合、エラー
+	return "redirect:/syserror";
+		}
+
+		//Itemエンティティの各フィールドの値をItemBeanにコピー
+		ItemBean itemBean = beanTools.copyEntityToItemBean(item);
+
+		// 商品情報をViewへ渡す
+		model.addAttribute("item", itemBean);
+		
+		
+	return "client/item/detail";
+	
+	}
 }

c984d811f7435b4ea2816a41a86477481cab325f
Yosikawa <s205257@stu.bunri-u.ac.jp>
2024-06-13 11:09:48 +0900

    動作確認

--- src/main/java/jp/co/sss/shop/controller/client/user/ClientUserShowController.java
+++ src/main/java/jp/co/sss/shop/controller/client/user/ClientUserShowController.java
@@ -2,6 +2,8 @@ package jp.co.sss.shop.controller.client.user;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,6 +35,39 @@ public class ClientUserShowController {
 	@Autowired
 	HttpSession session;
 
+	/**
+	 * 一覧表示処理
+	 *
+	 * @param model  Viewとの値受渡し
+	 * @param pageable ページング制御
+	 * @return "admin/user/list" 一覧画面　表示
+	 */
+	@RequestMapping(path = "/Client/user/list", method = { RequestMethod.GET, RequestMethod.POST })
+	public String showUserList(Model model, Pageable pageable) {
+
+		// 会員情報の登録数の取得と新規追加可否チェック
+		Long usersCount = userRepository.count();
+		Boolean registrable = true;
+		if (usersCount == Constant.USERS_MAX_COUNT) {
+			registrable = false;
+		}
+
+		// 会員情報リストを取得
+		Integer authority = ((UserBean) session.getAttribute("user")).getAuthority();
+		Page<User> userList = userRepository.findUsersListOrderByInsertDate(Constant.NOT_DELETED, authority, pageable);
+
+		// 会員情報をViewに渡す
+		model.addAttribute("registrable", registrable);
+		model.addAttribute("pages", userList);
+		model.addAttribute("users", userList.getContent());
+		
+
+		//会員登録・変更・削除用のセッションスコープを初期化
+		session.removeAttribute("userForm");
+
+		// 一覧表示
+		return "Client/user/list";
+	}
 
 	/**
 	 * 詳細表示処理
@@ -42,7 +77,7 @@ public class ClientUserShowController {
 	 * @return "admin/user/detail" 会員詳細表示画面へ
 	 * 
 	 */
-	@RequestMapping(path = "/client/user/detail", method = { RequestMethod.GET, RequestMethod.POST })
+	@RequestMapping(path = "/Client/user/detail/{id}", method = { RequestMethod.GET, RequestMethod.POST })
 	public String showUser(@PathVariable int id, Model model) {
 		// 表示対象の情報を取得
 		User user = userRepository.findByIdAndDeleteFlag(id, Constant.NOT_DELETED);
@@ -62,7 +97,8 @@ public class ClientUserShowController {
 		session.removeAttribute("userForm");
 
 		// 詳細画面　表示
-		return "client/user/detail";
+		return "Client/user/detail";
 	}
 }
+
 

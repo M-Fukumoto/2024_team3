32d7814305386f5a777785eca0992c50fe6446c3
naoya <nattou708@gmail.com>
2024-06-18 10:05:37 +0900

    order

--- src/main/java/jp/co/sss/shop/repository/OrderItemRepository.java
+++ src/main/java/jp/co/sss/shop/repository/OrderItemRepository.java
@@ -13,11 +13,6 @@ import jp.co.sss.shop.entity.OrderItem;
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
 	
-	/*OrderItem findById(Integer id); 
-	List<OrderItem> findAllByOrderItemByInsertDateDescPage(Integer userId);
-	
-	@Query(JPQLConstant.FIND_ORDERITEMS_LIST_ORDER_BY_INSERT_DATE)
-	Page<User> findUsersListOrderByInsertDate(
-			@Param("deleteFlag") int deleteFlag, @Param("authority") int authority, Pageable pageable);
-			*/
+	//List<Orders> findAllByOrderItemByInsertDateDesc(Integer userId); 
+			
 }
